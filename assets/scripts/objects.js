34c1d1194f29a7dc4da289113a7ee5602080927a
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2023-10-04 22:25:37 +0200

    Update objects.js

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,3 +1,4 @@
+/*
 const movieList = document.getElementById('movie-list');
 
 movieList.style.backgroundColor= 'magenta';
@@ -20,3 +21,59 @@ person.isAdmin = true;
 console.log (person ['first name']);
 console.log(person[1.5]);
 console.log(person);
+*/
+
+const addMovieBtn = document.getElementById ('add-movie-btn');
+const searchBtn = document.getElementById ('search-btn');
+
+const movies= [];
+
+const renderMovies= ()=> {
+  const movieList= document.getElementById('movie-list');
+
+  
+  if (movies.lenght===0){
+    movieList.classList.remove('visible');
+    return;
+  }else {
+    movieList.classList.add('visible');
+  }
+  movieList.innerHTML = '';
+
+  movies.forEach((movie)=>{
+    const movieEl = document.createElement('li');
+    movieEl.textContent = movie.info.title;
+    movieList.append(movieEl); 
+  });
+};
+
+
+const addMovieHandler = () => {
+const title = document.getElementById ('title').value;
+const extraName = document.getElementById ('extra-name').value;
+const extraValue = document. getElementById('extra-value').value;
+
+
+
+if (
+  title.trim()===''||
+  extraName.trim() ==='' ||
+  extraValue.trim()=== ''
+    ) {
+      return;
+    }
+
+    const newMovie = {
+
+      info: {
+        title,
+        [extraName]:extraValue
+
+    },
+id: Math.random()
+};
+movies.push(newMovie);
+renderMovies();
+};
+
+addMovieBtn.addEventListener('click', addMovieHandler);
